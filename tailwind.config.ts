2dc4e08683b1eed4b57be949e1bee4c1c9995d27
PTBYSR <paul.emechebe@gmail.com>
2023-09-07 17:16:18 +0100

    added font files and font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,14 +13,21 @@ const config: Config = {
       'text-main': "#353535",
       "text-gray": "#979797",
       "text-light": "#DDDDDD"
-    }, 
+    },
     boxShadow: {
       sm: '0px 20px 50px 0px rgba(203, 203, 203, 0.46)',
       lg: "0px 14.569314956665039px 36.42328643798828px 0px rgba(203, 203, 203, 0.46)",
 
     },
     fontSize: {
-
+      '4xl': '60px',
+      '3xl': '48px',
+      '2xl': '34px',
+      xl: '24px',
+      lg: '20px',
+      sm: '16px',
+      xs: '14px',
+      xxs: '12px'
     },
     fontFamily: {
       roboto: 'Roboto, sans-serif',
@@ -32,8 +39,12 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-    
+
   },
   plugins: [],
 }
 export default config
+
+
+
+
